9361f8907d8cd1d52f546d85d38ba35889fd6303
Anderson <Matthew.Anderson@bostonbeer.com>
2017-07-24 09:41:23 -0400

    restore
    Added restore.

---
+++ Restore-SSASDatabaseScript.ps1
@@ -0,0 +1,23 @@
+﻿Function Restore-SSASDatabaseScript
+{
+
+    [CmdletBinding()]
+	Param (
+            [parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
+            [object]$sqlServer,
+            [object]$path
+        )
+    
+    Import-Module sqlserver;
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices") | Out-Null
+
+    #Connect to the server. 
+    $SSASServer = New-Object Microsoft.AnalysisServices.Server 
+    $SSASServer.Connect($sqlServer)     
+
+    Try{Invoke-ASCmd -InputFile $path -Server $SSASServer}
+    Catch{Write-Host $_.Exception.Message}
+
+}#End Restore-SSASDatabaseScript
+
+Restore-SSASDatabaseScript -sqlServer Server1 -path "C:\Users\user\SSAS_backups\databaseBackup.xmla"
